e8584197346f3b21cc86d60bc44f769a5e9ad8b4
Cameron Morgan <hello@cameron-morgan.com>
2023-11-29 19:29:50 +1000

    Update musicbox.py

--- musicbox.py
+++ musicbox.py
@@ -2,8 +2,12 @@
 from pysqueezebox import Server, Player
 import aiohttp
 import asyncio
-import functools
 import RPi.GPIO as GPIO
+
+# OLED Display
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.oled.device import ssd1322
 
 # Define input pins
 BUTTON_VOL = 13
@@ -17,11 +21,22 @@ TUNER_B = 9
 LMS_SERVER = '10.0.1.100'
 PLAYER_NAME = 'Radio'
 
+# Setup OLED
+serial = spi(device=0, port=0)
+device = ssd1322(serial)
+
+# Case I/O
 def pause(channel):
     print("Pausing stream.")
 
 def tuner(channel):
     print("Tuner pressed.")
+
+def volume_encoder(channel):
+    print("Encoder engaged.")
+
+def tuner_encoder(channel):
+    print("Encoder engaged.")
 
 async def setup():
     async with aiohttp.ClientSession() as session:
@@ -40,10 +55,16 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(BUTTON_VOL, GPIO.IN)
     GPIO.setup(BUTTON_TUNER, GPIO.IN)
+    GPIO.setup(VOL_A, GPIO.IN)
+    GPIO.setup(VOL_B, GPIO.IN)
+    GPIO.setup(TUNER_A, GPIO.IN)
+    GPIO.setup(TUNER_B, GPIO.IN)
 
     # Event listeners
     GPIO.add_event_detect(BUTTON_VOL, GPIO.RISING, callback=pause, bouncetime=500)
     GPIO.add_event_detect(BUTTON_TUNER, GPIO.RISING, callback=tuner, bouncetime=500)
+    GPIO.add_event_detect(VOL_A, GPIO.RISING, callback=volume_encoder, bouncetime=10)
+    GPIO.add_event_detect(TUNER_A, GPIO.RISING, callback=tuner_encoder, bouncetime=10)
 
     main_loop = asyncio.get_event_loop()
     main_loop.run_forever()
