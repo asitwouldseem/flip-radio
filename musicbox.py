daa9bf10d18ffc8cecd7560757aaaeac6e850083
Cameron Morgan <hello@cameron-morgan.com>
2023-11-29 19:07:57 +1000

    Update musicbox.py

--- musicbox.py
+++ musicbox.py
@@ -1,28 +1,56 @@
 #!/usr/bin/python3
 from pysqueezebox import Server, Player
-import RPi.GPIO as GPIO
 import aiohttp
 import asyncio
+import functools
+import RPi.GPIO as GPIO
 
-SERVER = '10.0.1.100'
-VOL_A = 5
+# Define input pins
+BUTTON_VOL = 13
+BUTTON_TUNER = 0
+VOL_A = 6
 VOL_B = 22
-VOL_BUTTON = 13
+TUNER_A = 5
+TUNER_B = 9
 
-# Setup GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(VOL_A, GPIO.IN)
-GPIO.setup(VOL_B, GPIO.IN)
-GPIO.setup(VOL_BUTTON, GPIO.IN)
+# Define LMS settings
+LMS_SERVER = '10.0.1.100'
+PLAYER_NAME = 'Radio'
 
-async def main():
-  async with aiohttp.ClientSession() as session:
-    lms = Server(session, SERVER)
-    player = await lms.async_get_player(name="Radio")
+def pause(channel):
+    print("Pausing stream.")
 
-    await player.async_update()
-    print(player.album)
-    await player.async_play()
+def tuner(channel):
+    print("Tuner pressed.")
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+async def setup():
+    async with aiohttp.ClientSession() as session:
+      lms = Server(session, LMS_SERVER)
+      player = await lms.async_get_player(name=PLAYER_NAME)
+      await player.async_update()
+
+if __name__ == '__main__':
+  try:
+    # Create and run the event loop for setup
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(setup())
+
+    # Now set up GPIO and the main event loop
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_VOL, GPIO.IN)
+    GPIO.setup(BUTTON_TUNER, GPIO.IN)
+
+    # Event listeners
+    GPIO.add_event_detect(BUTTON_VOL, GPIO.RISING, callback=pause, bouncetime=500)
+    GPIO.add_event_detect(BUTTON_TUNER, GPIO.RISING, callback=tuner, bouncetime=500)
+
+    main_loop = asyncio.get_event_loop()
+    main_loop.run_forever()
+
+  except KeyboardInterrupt:
+    pass
+  except Exception as e:
+    print(f"Error: {e}")
+  finally:
+    GPIO.cleanup()
