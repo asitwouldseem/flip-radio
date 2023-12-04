9bd02b3289bfaee196e30d8f820b5a95d5a70bca
Cameron Morgan <hello@cameron-morgan.com>
2023-12-04 16:02:31 +1000

    Add working transport control, start programming encoders

--- musicbox.py
+++ musicbox.py
@@ -1,82 +1,96 @@
 #!/usr/bin/python3
-from pysqueezebox import Server, Player
+from pysqueezebox import Server
 import aiohttp
 import asyncio
 import RPi.GPIO as GPIO
-
-# OLED Display
-from luma.core.interface.serial import spi
-from luma.core.render import canvas
-from luma.oled.device import ssd1322
+from time import sleep
 
 # Define input pins
 BUTTON_VOL = 13
 BUTTON_TUNER = 0
-VOL_A = 6
-VOL_B = 22
-TUNER_A = 5
-TUNER_B = 9
+VOL_CLK = 6
+VOL_DT = 22
 
 # Define LMS settings
 LMS_SERVER = '10.0.1.100'
 PLAYER_NAME = 'Radio'
 
-# Setup OLED
-serial = spi(device=0, port=0)
-device = ssd1322(serial)
+# Global variables for rotary encoder
+vol_counter = 0
+volClkLast = None
 
 # Case I/O
 def pause(channel):
-    print("Pausing stream.")
+    loop.run_in_executor(None, lambda: asyncio.run(pauseTrack()))
+    
+    # Print to terminal for debugging
+    print("Transport command sent.")
 
 def tuner(channel):
-    print("Tuner pressed.")
+    loop.run_in_executor(None, lambda: asyncio.run(pauseTrack()))
 
 def volume_encoder(channel):
-    print("Encoder engaged.")
+    global vol_counter, volClkLast
 
-def tuner_encoder(channel):
-    print("Encoder engaged.")
+    volClk = GPIO.input(VOL_CLK)
+    volDt = GPIO.input(VOL_DT)
+    
+    if volClkLast is not None:
+        if volClk != volClkLast:
+            if volDt != volClk:
+                vol_counter += 1
+            else:
+                vol_counter -= 1
+            print(vol_counter)
+    
+    volClkLast = volClk
+    sleep(0.01)
 
 async def pauseTrack():
-  async with aiohttp.ClientSession() as session:
-    lms = Server(session, LMS_SERVER)
-    player = await lms.async_get_player(name=PLAYER_NAME)
-    await player.async_update()
-    print(player.album)
+    async with aiohttp.ClientSession() as session:
+        lms = Server(session, LMS_SERVER)
+        player = await lms.async_get_player(name=PLAYER_NAME)
+        await player.async_toggle_pause()
+
+async def setVolume(volume):
+    async with aiohttp.ClientSession() as session:
+        lms = Server(session, LMS_SERVER)
+        player = await lms.async_get_player(name=PLAYER_NAME)
+        await player.async_set_volume(volume)
 
 async def setup():
     async with aiohttp.ClientSession() as session:
-      lms = Server(session, LMS_SERVER)
+        lms = Server(session, LMS_SERVER)
+        player = await lms.async_get_player(name=PLAYER_NAME)
 
 if __name__ == '__main__':
-  try:
-    # Create and run the event loop for setup
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(setup())
+    try:
+        # Create and run the event loop for setup
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(setup())
 
-    # Now set up GPIO and the main event loop
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_VOL, GPIO.IN)
-    GPIO.setup(BUTTON_TUNER, GPIO.IN)
-    GPIO.setup(VOL_A, GPIO.IN)
-    GPIO.setup(VOL_B, GPIO.IN)
-    GPIO.setup(TUNER_A, GPIO.IN)
-    GPIO.setup(TUNER_B, GPIO.IN)
+        # Now set up GPIO and the main event loop
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(BUTTON_VOL, GPIO.IN)
+        GPIO.setup(BUTTON_TUNER, GPIO.IN)
+        GPIO.setup(VOL_CLK, GPIO.IN)
+        GPIO.setup(VOL_DT, GPIO.IN)
 
-    # Event listeners
-    GPIO.add_event_detect(BUTTON_VOL, GPIO.RISING, callback=pause, bouncetime=500)
-    GPIO.add_event_detect(BUTTON_TUNER, GPIO.RISING, callback=tuner, bouncetime=500)
-    GPIO.add_event_detect(VOL_A, GPIO.RISING, callback=volume_encoder, bouncetime=10)
-    GPIO.add_event_detect(TUNER_A, GPIO.RISING, callback=tuner_encoder, bouncetime=10)
+        # Rotary encoders
+        volClkLast = GPIO.input(VOL_CLK)
 
-    main_loop = asyncio.get_event_loop()
-    main_loop.run_forever()
+        # Event listeners
+        GPIO.add_event_detect(BUTTON_VOL, GPIO.RISING, callback=pause, bouncetime=500)
+        GPIO.add_event_detect(BUTTON_TUNER, GPIO.RISING, callback=tuner, bouncetime=500)
+        GPIO.add_event_detect(VOL_CLK, GPIO.BOTH, callback=volume_encoder, bouncetime=10)
 
-  except KeyboardInterrupt:
-    pass
-  except Exception as e:
-    print(f"Error: {e}")
-  finally:
-    GPIO.cleanup()
+        main_loop = asyncio.get_event_loop()
+        main_loop.run_forever()
+
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        GPIO.cleanup()
