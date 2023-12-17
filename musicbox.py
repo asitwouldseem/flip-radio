fa13337798ea1e15c7f571745ff62c5b7ae503a9
Cameron Morgan <hello@cameron-morgan.com>
2023-12-17 17:09:48 +1000

    Add second encoder button for power control

--- musicbox.py
+++ musicbox.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+from PIL import ImageFont
 from pysqueezebox import Server
+import RPi.GPIO as GPIO
 import aiohttp
 import asyncio
 import time
-import RPi.GPIO as GPIO
 
 # OLED display
 from luma.core.interface.serial import spi
@@ -12,6 +13,7 @@ from luma.oled.device import ssd1322
 
 # Define input pins
 BUTTON_VOL = 13
+BUTTON_TUNER = 0
 VOL_CLK = 6
 VOL_DT = 22
 CLOCK_LED = 23
@@ -24,6 +26,9 @@ PLAYER_NAME = 'Radio'
 serial = spi(device=0, port=0)
 device = ssd1322(serial)
 
+type1 = ImageFont.truetype('/home/cameron/fonts/WorkSans-SemiBold.ttf', 25)
+type2 = ImageFont.truetype('/home/cameron/fonts/WorkSans-Medium.ttf', 14)
+
 # Rotary encoders
 vol_counter = 0
 vol_change = 2
@@ -32,14 +37,16 @@ volClkLast = None
 # Case I/O
 def pause(channel):
     loop.run_in_executor(None, lambda: asyncio.run(pauseTrack()))
-    print("Transport control pressed.")
+
+def tuner(channel):
+    loop.run_in_executor(None, lambda: asyncio.run(powerDevice()))
 
 def volume_encoder(channel):
     global vol_counter, volClkLast
 
     volClk = GPIO.input(VOL_CLK)
     volDt = GPIO.input(VOL_DT)
-    
+
     if volClkLast is not None:
         if volClk != volClkLast:
             if volDt != volClk:
@@ -60,11 +67,18 @@ def clear_display():
         draw.rectangle(device.bounding_box, outline="black", fill="black")
 
 # Interactions with Logitech Media Server
-async def currentTrack():
+async def powerDevice():
     async with aiohttp.ClientSession() as session:
         lms = Server(session, LMS_SERVER)
         player = await lms.async_get_player(name=PLAYER_NAME)
-        return await player.title()
+        await player.async_update()
+
+        if player.power == 1: 
+            await player.async_set_power(0)
+            print("Goodbye.")
+        else:
+            await player.async_set_power(1)
+            print("Hello.")
 
 async def pauseTrack():
     async with aiohttp.ClientSession() as session:
@@ -76,7 +90,6 @@ async def currentVolume():
     async with aiohttp.ClientSession() as session:
         lms = Server(session, LMS_SERVER)
         player = await lms.async_get_player(name=PLAYER_NAME)
-        return await player.volume()
 
 async def setVolume(volume):
     async with aiohttp.ClientSession() as session:
@@ -90,7 +103,7 @@ async def setup():
         player = await lms.async_get_player(name=PLAYER_NAME)
 
         with canvas(device) as draw:
-          draw.text((10, 40), "Connection established.", fill="white")
+          draw.text((10, 50), "Connection established.", font=type2, fill="white")
 
         # If we're not playing anything, it'll show this forever. Lame.
         time.sleep(5)
@@ -98,6 +111,7 @@ async def setup():
 
 async def main():
     last_mode = 'stop'
+    last_title = None
 
     while True:
         async with aiohttp.ClientSession() as session:
@@ -106,12 +120,17 @@ async def main():
             await player.async_update()
 
             if player.mode == 'play' and last_mode != 'play':
-                # Control Clock LED.
                 GPIO.output(CLOCK_LED,GPIO.HIGH)
 
-                # Print song information to OLED.
-                with canvas(device) as draw:
-                    draw.text((5, 25), "Hello World", fill="white")
+                if player.title: 
+                    if player.title != last_title:
+                        with canvas(device) as draw:
+                            draw.text((5, 10), player.title, font=type1, fill="white")
+                            draw.text((5, 45), player.artist, font=type2, fill="white")
+                        last_title = player.title
+                else:
+                    with canvas(device) as draw:
+                        draw.text((10, 50), "No track information.", font=type2, fill="white")
 
             elif player.mode == 'stop':
                 GPIO.output(CLOCK_LED,GPIO.LOW)
@@ -131,6 +150,7 @@ if __name__ == '__main__':
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(CLOCK_LED, GPIO.OUT)
         GPIO.setup(BUTTON_VOL, GPIO.IN)
+        GPIO.setup(BUTTON_TUNER, GPIO.IN)
         GPIO.setup(VOL_CLK, GPIO.IN)
         GPIO.setup(VOL_DT, GPIO.IN)
 
@@ -139,6 +159,7 @@ if __name__ == '__main__':
 
         # Event listeners
         GPIO.add_event_detect(BUTTON_VOL, GPIO.RISING, callback=pause, bouncetime=500)
+        GPIO.add_event_detect(BUTTON_TUNER, GPIO.RISING, callback=tuner, bouncetime=500)
         GPIO.add_event_detect(VOL_CLK, GPIO.BOTH, callback=volume_encoder, bouncetime=10)
 
         # Run the main loop
