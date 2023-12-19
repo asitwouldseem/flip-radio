98d88baedcd867b0d1cb7942c6b5e3e1db7fee49
Cameron Morgan <hello@cameron-morgan.com>
2023-12-19 14:05:08 +1000

    Add nowplaying() function, check current volume before changing.

--- musicbox.py
+++ musicbox.py
@@ -16,6 +16,8 @@ BUTTON_VOL = 13
 BUTTON_TUNER = 0
 VOL_CLK = 6
 VOL_DT = 22
+TUNER_CLK = 5
+TUNER_DT = 9
 CLOCK_LED = 23
 
 # Define LMS settings
@@ -39,8 +41,9 @@ font_info = ImageFont.truetype('/home/cameron/fonts/WorkSans-Light.ttf', 12)
 
 # Rotary encoders
 vol_counter = 0
-vol_change = 2
+vol_change = 5
 volClkLast = None
+tunerClkLast = None
 
 # Case I/O
 def pause(channel):
@@ -58,21 +61,52 @@ def volume_encoder(channel):
     if volClkLast is not None:
         if volClk != volClkLast:
             if volDt != volClk:
-                vol_counter += vol_change
+                vol_counter = vol_change
             else:
-                vol_counter -= vol_change
+                vol_counter = -vol_change
+        else:
+            vol_counter = 0
     
-    vol_counter = max(0, min(vol_counter, 100))
     volClkLast = volClk
 
-    loop.run_in_executor(None, lambda: asyncio.run(setVolume(vol_counter)))
+    # Pass encoder value to setVolume to work out the change.
+    loop.call_later(0.5, lambda: asyncio.create_task(setVolume(vol_counter)))
 
-    # Print to terminal for debugging
-    print(f"Volume changed to {vol_counter}")
+def tuner_encoder(channel):
+    global tuner_counter, tunerClkLast
+
+    tunerClk = GPIO.input(TUNER_CLK)
+    tunerDt = GPIO.input(TUNER_DT)
+
+    #TODO: Add rotary encoder to control... something.
 
 def clear_display():
-    with canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline="black", fill="black")
+    device.clear()
+
+# We call this function to update the OLED with track information.
+async def nowPlaying():
+    last_title = None
+
+    async with aiohttp.ClientSession() as session:
+        lms = Server(session, LMS_SERVER)
+        player = await lms.async_get_player(name=PLAYER_NAME)
+        await player.async_update()
+
+        # We'll only update when the track changes.
+        if player.title != last_title:
+            if player.title:
+                with canvas(device) as draw:
+                    draw.text((left, middle), player.title, font=font_title, anchor="ls", fill="white")
+                    draw.text((left, middle + 15), player.artist, font=font_artist, anchor="ls", fill="white")
+            else:
+                with canvas(device) as draw:
+                    draw.text((left, middle), "No track information.", anchor="ls", font=font_info, fill="white")
+
+        # This shouldn't get called... but just in case LMS is doing something odd.
+        else:
+            clear_display()
+
+        last_title = player.title
 
 # Interactions with Logitech Media Server
 async def powerDevice():
@@ -83,10 +117,11 @@ async def powerDevice():
 
         if player.power == 1: 
             await player.async_set_power(0)
-            print("Goodbye.")
         else:
             await player.async_set_power(1)
-            print("Hello.")
+
+            # This makes sure the Now Playing display reappears after a manual power off.
+            loop.run_in_executor(None, lambda: asyncio.run(nowPlaying()))
 
 async def pauseTrack():
     async with aiohttp.ClientSession() as session:
@@ -94,16 +129,22 @@ async def pauseTrack():
         player = await lms.async_get_player(name=PLAYER_NAME)
         await player.async_toggle_pause()
 
-async def currentVolume():
+async def setVolume(encoder_value):
     async with aiohttp.ClientSession() as session:
         lms = Server(session, LMS_SERVER)
         player = await lms.async_get_player(name=PLAYER_NAME)
+        await player.async_update()
 
-async def setVolume(volume):
-    async with aiohttp.ClientSession() as session:
-        lms = Server(session, LMS_SERVER)
-        player = await lms.async_get_player(name=PLAYER_NAME)
-        await player.async_set_volume(volume)
+        # Add encoder value to volume.
+        new_volume = player.volume + encoder_value
+        new_volume = max(0, min(new_volume, 100))
+
+        # Send to LMS.
+        await player.async_set_volume(new_volume)
+
+        # Display on OLED.
+        with canvas(device) as draw:
+            draw.text((left, middle), "Volume: " + str(new_volume), anchor="ls", font=font_info, fill="white")
 
 async def setup():
     async with aiohttp.ClientSession() as session:
@@ -118,31 +159,21 @@ async def setup():
         clear_display()
 
 async def main():
-    last_title = None
-
     while True:
         async with aiohttp.ClientSession() as session:
             lms = Server(session, LMS_SERVER)
             player = await lms.async_get_player(name=PLAYER_NAME)
             await player.async_update()
 
-            if player.title != last_title:
-                if player.mode == 'play':
-                    GPIO.output(CLOCK_LED,GPIO.HIGH)
+            if player.mode == 'play':
+                GPIO.output(CLOCK_LED,GPIO.HIGH)
+                loop.run_in_executor(None, lambda: asyncio.run(nowPlaying()))
 
-                    if player.title:
-                        with canvas(device) as draw:
-                            draw.text((left, middle), player.title, font=font_title, anchor="ls", fill="white")
-                            draw.text((left, middle + 15), player.artist, font=font_artist, anchor="ls", fill="white")
-                    else:
-                        with canvas(device) as draw:
-                            draw.text((left, middle), "No track information.", anchor="ls", font=font_info, fill="white")
+            # If we've stopped playback. Put away the fancy stuff.
+            if player.mode == 'stop':
+                GPIO.output(CLOCK_LED,GPIO.LOW)
+                clear_display()
 
-                elif player.mode == 'stop':
-                    GPIO.output(CLOCK_LED,GPIO.LOW)
-                    clear_display()
-
-            last_title = player.title
             await asyncio.sleep(1)
 
 if __name__ == '__main__':
@@ -159,6 +190,8 @@ if __name__ == '__main__':
         GPIO.setup(BUTTON_TUNER, GPIO.IN)
         GPIO.setup(VOL_CLK, GPIO.IN)
         GPIO.setup(VOL_DT, GPIO.IN)
+        GPIO.setup(TUNER_CLK, GPIO.IN)
+        GPIO.setup(TUNER_DT, GPIO.IN)
 
         # Rotary encoders
         volClkLast = GPIO.input(VOL_CLK)
@@ -166,7 +199,8 @@ if __name__ == '__main__':
         # Event listeners
         GPIO.add_event_detect(BUTTON_VOL, GPIO.RISING, callback=pause, bouncetime=500)
         GPIO.add_event_detect(BUTTON_TUNER, GPIO.RISING, callback=tuner, bouncetime=500)
-        GPIO.add_event_detect(VOL_CLK, GPIO.BOTH, callback=volume_encoder, bouncetime=10)
+        GPIO.add_event_detect(VOL_CLK, GPIO.BOTH, callback=volume_encoder, bouncetime=225)
+        GPIO.add_event_detect(TUNER_CLK, GPIO.BOTH, callback=tuner_encoder, bouncetime=225)
 
         # Run the main loop
         loop.run_until_complete(main())
